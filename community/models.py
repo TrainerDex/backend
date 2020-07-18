d4910f818de5298be6e9db5abeeecd8ad21a3e07
Jay Turner <jay@trainerdex.co.uk>
2020-07-18 16:53:17 +0100

    Make a start on adding Communites.

--- community/models.py
+++ community/models.py
@@ -1,56 +1,77 @@
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
-from django.utils.translation import gettext_lazy as _, npgettext_lazy
+from django.utils.translation import gettext_lazy as _, npgettext_lazy, pgettext_lazy
+from django_countries.fields import CountryField
+
 from pytz import common_timezones
 
+from trainerdex.leaderboard import Leaderboard
 from trainerdex.models import Trainer
 
 
 class Community(models.Model):
     handle = models.SlugField(
         primary_key=True,
-        editable=False,
+        verbose_name=pgettext_lazy("community__handle__title", "Handle"),
+        help_text=pgettext_lazy("community__handle__help", "Once you set this, it cannot be changed. Pick carefully as this will be the identifier to find your community leaderboard."),
     )
     language = models.CharField(
         default=settings.LANGUAGE_CODE,
         choices=settings.LANGUAGES,
         max_length=len(max(dict(settings.LANGUAGES).keys(), key=len)),
+        verbose_name=_("language"),
     )
     timezone = models.CharField(
         default=settings.TIME_ZONE,
         choices=((x, x) for x in common_timezones),
         max_length=len(max(common_timezones, key=len)),
+        verbose_name=_("timezone")
     )
+    country = CountryField(
+        null=True,
+        blank=True,
+        verbose_name=pgettext_lazy("community__country__title", "country"),
+        help_text=pgettext_lazy("community__country__help", "Where your community is based"),
+    )
+    
     name = models.CharField(
         max_length=70,
+        verbose_name=pgettext_lazy("community__name__title", "name"),
+        help_text=pgettext_lazy("community__name__help", "Max 70 characters"),
     )
     description = models.TextField(
         null=True,
         blank=True,
+        verbose_name=pgettext_lazy("community__description__title", "description"),
+        help_text=pgettext_lazy("community__description__help", "What makes your community unique? What are you about?"),
     )
 
-    privacy_public = models.BooleanField(
+    can_see = models.BooleanField(
         default=False,
-        verbose_name=_("Publicly Viewable"),
-        help_text=_("By default, this is off. Turn this on to share your community with the world."),
+        verbose_name=pgettext_lazy("community__can_see__title", "Publicly Viewable"),
+        help_text=pgettext_lazy("community__can_see__help","Default: False\nTurn this on to share your community with the world."),
     )
-    privacy_public_join = models.BooleanField(
+    can_join = models.BooleanField(
         default=False,
-        verbose_name=_("Publicly Joinable"),
-        help_text=_("By default, this is off. Turn this on to make your community free to join. No invites required."),
+        verbose_name=pgettext_lazy("community__can_join__title", "Publicly Joinable"),
+        help_text=pgettext_lazy("community__can_join__title", "Default: False\nTurn this on to make your community free to join. No invites required."),
     )
 
     members = models.ManyToManyField(
         Trainer,
         blank=True,
     )
-
+    
+    def country_flag(self):
+        return self.country.unicode_flag
+    country_flag.short_description = country.verbose_name
+    
+    def get_leaderboard(self, legacy_mode: bool = False, order_by: str = 'total_xp'):
+        return Leaderboard(legacy_mode=legacy, order_by=order_by, queryset=self.members)
+    
     def __str__(self) -> str:
         return self.name
-    
-    def get_absolute_url(self) -> str:
-        return reverse('trainerdex:leaderboard', kwargs={'community': self.handle})
     
     class Meta:
         verbose_name = npgettext_lazy("community__title", "community", "communities", 1)
--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'rest_framework',
     'rest_framework.authtoken',
-    # 'community',
+    'community',
     'core',
     'trainerdex',
     'allauth',
